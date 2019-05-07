ed58cbe48123de770c487fc7109e8b079dfe7436
Eric Mauldin <emauldin84@gmail.com>
2019-05-07 09:49:48 -0400

    adds skeleton of redux code with pokemon deck

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import initialState from './base.json'
+
+console.log(initialState);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
